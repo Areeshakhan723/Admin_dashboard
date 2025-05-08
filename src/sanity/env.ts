5a2704f1b3b36dae17a6622f1355d53fdcfbb8de
Areeshakhan723 <areshakhan6400@gmail.com>
2025-05-08 19:00:24 +0500

    some changes

--- src/sanity/env.ts
+++ src/sanity/env.ts
@@ -12,8 +12,8 @@ export const projectId = assertValue(
 )
 
 export const token = assertValue(
-  "sksM7K82fOBUD9qBKo8IRKLGUSp1e2zeg8vHv4pT8QIP0nEQDhAd31DAHYGhy0AKXSQrPz5oeMYzsnDf0PZdkoHAlwPQQNewSB60S4FH60EEsQnrZCN0mKL19otped0q3YqRchMmx3WDFbe0PEy42unLEbSMkKiDPnpruuVmRaop0ysCkWVn",
-  'Missing environment variable: NEXT_API_TOKEN'
+  process.env.SANITY_API_TOKEN,
+  'Missing environment variable: SANITY_API_TOKEN'
 )
 
 function assertValue<T>(v: T | undefined, errorMessage: string): T {
